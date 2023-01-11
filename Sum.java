31c264411c9cfd8c81c2f62bf8100cb095c27049
Abdul Malik <Iamjunayid@gmail.com>
2023-01-11 19:19:21 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -1,8 +1,8 @@
 public class Sum {
 
     public static void main(String args[]) {
-        int a = 5;
-        int b = 10;
+        int a = 6;
+        int b = 9;
 
         System.out.println("The Sum is " + (a + b));
     }
